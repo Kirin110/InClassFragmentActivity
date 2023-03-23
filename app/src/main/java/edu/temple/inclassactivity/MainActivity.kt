934114e741b6b7b87490918243b23f8bd131b691
Kirin <tuj17819@temple.edu>
2023-03-23 12:19:27 -0400

    Added a view model

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,24 +2,32 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
 
+        mainViewModel.setImageIds(imageArray)
+
         // Attach an instance of ImageDisplayFragment using factory method
         //Passes in imageArray as its argument
         //Adds fragment to the fragment container view using a fragment transaction
         //Then commits it
-        val imageDisplayFragment = ImageDisplayFragment.newInstance(imageArray)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainerView, imageDisplayFragment)
-            .commit()
+        //val imageDisplayFragment = ImageDisplayFragment.newInstance(imageArray)
+        if(supportFragmentManager.findFragmentById(R.id.fragmentContainerView) !is ImageDisplayFragment) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView, ImageDisplayFragment())
+                .commit()
+        }
+
     }
 }
